b3e77e7759d444e756d00556512587f08475977c
edoardob90 <edoardob90@gmail.com>
2018-09-18 14:24:19 +0200

    Exercise on Python

--- exercises/week1/python-hello/sources/hello.py
+++ exercises/week1/python-hello/sources/hello.py
@@ -1,4 +1,7 @@
 import sys
+import series.py
 
-print("Hello World!")
-print(len(sys.argv))
+print("Hello World!") # print "Hello"
+#print(len(sys.argv),type(sys.argv)) # check the type of sys.argv
+print("Your number is ",int(sys.argv[1]))
+computeSeries(sys.argv[1])
