44ac7b6e92f6a0f86150d9ec1ec3330cdf7a3a13
edoardob90 <edoardob90@gmail.com>
2018-09-18 14:32:13 +0200

    Second exercise with Python

--- exercises/week1/python-hello/sources/hello.py
+++ exercises/week1/python-hello/sources/hello.py
@@ -1,7 +1,8 @@
 import sys
-import series.py
+import series
 
 print("Hello World!") # print "Hello"
 #print(len(sys.argv),type(sys.argv)) # check the type of sys.argv
 print("Your number is ",int(sys.argv[1]))
-computeSeries(sys.argv[1])
+series.computeSeries(sys.argv[1])
+
---
+++ exercises/week1/python-hello/sources/series.py
@@ -0,0 +1,5 @@
+def computeSeries(N):
+    mysum = 0
+    for i in range(int(N)):
+        mysum += i
+    print("Your sum is ",mysum)
