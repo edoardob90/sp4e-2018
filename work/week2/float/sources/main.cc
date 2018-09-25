202e824d2b744e1dd4d4d869cf90ec286c4de3fd
edoardob90 <edoardob90@gmail.com>
2018-09-25 16:02:07 +0200

    Exercise 2: float numbers

--- work/week2/float/sources/main.cc
+++ work/week2/float/sources/main.cc
@@ -4,12 +4,21 @@
 #include <iostream>
 /* -------------------------------------------------------------------------- */
 
+using namespace std;
+
 void exo1(){
-  
+    cerr << "Exercise 1" << endl;
+    //int a = 0.5;
+    //cout << a << endl;
+    double a = 1/2;
+    cout << a << endl;
 
 }
 /* -------------------------------------------------------------------------- */
 void exo2(){
+    double x = 10.;
+    double a = exp(x);
+    cout << a << endl;
 }
 /* -------------------------------------------------------------------------- */
 void exo3(){
@@ -20,9 +29,9 @@ void exo3(){
 int main(int argc, char ** argv){
 
 
-  exo1();
+  //exo1();
   exo2();
-  exo3();
+  //exo3();
 
   return EXIT_SUCCESS;
 }
