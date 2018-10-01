ab94a8c98f492a92e453a73ed31086f6b5bf1264
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2018-10-01 23:30:00 +0200

    adding the snippets

---
+++ lectures/code_snippets/fstream.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+
+int main() {
+  std::ofstream fout("test.plot");
+
+  fout << M_PI << std::endl;
+}
---
+++ lectures/code_snippets/unique_pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+
+std::unique_ptr<double> get_scalar() { return std::make_unique<double>(3); }
+
+int main() {
+
+  std::unique_ptr<double> ptr = get_scalar();
+  // ... do what I need like...
+  std::cout << *ptr;
+  // no need to delete scalar (will be automatically)
+
+  // cannot be copied => compilation error
+  // std::unique_ptr<double> ptr_copy = ptr;
+}
